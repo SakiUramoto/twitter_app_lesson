0a7f2c73c39fa42094558f8be5d7c2548b55251f
SakiUramoto <77236682+SakiUramoto@users.noreply.github.com>
2021-05-21 22:44:46 +0900

    ログインユーザーAvatar

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { auth } from "../firebase";
+import TweetInput from "./TweetInput";
+
 
 const Feed = () => {
   return (
     <div>
       Feed
+      <TweetInput/>
       <button onClick={() => auth.signOut()}>Logout</button>
+      
     </div>
   );
 };
